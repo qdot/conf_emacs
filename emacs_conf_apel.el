df7649f6cd32af99ec8fab77cc5fa19e0499f1d7
Kyle Machulis <kyle@nonpolynomial.com>
2010-03-29 22:51:39 -0700

    Fixed apel includes to work with new git submodules

--- emacs_conf_apel.el
+++ emacs_conf_apel.el
@@ -1,7 +1,13 @@
 ;; Setting up load directories for apel/flim/semi that a lot of japanese emacs modules use
 
-(add-to-list 'load-path (expand-file-name "~/.emacs_files/elisp_src/apel-10.7"))
-;; ;;require emacs mailcap before flim's mailcap
-;; (require 'mailcap)
-;; (add-to-list 'load-path (expand-file-name "~/.emacs_files/elisp_src/flim-1.14.6"))
-;; (add-to-list 'load-path (expand-file-name "~/.emacs_files/elisp_src/semi-1.14.6"))
+(when (file-exists-p (concat emacs-repo-elisp-submodule-dir "apel/"))
+  (add-to-list 'load-path (concat emacs-repo-elisp-submodule-dir "apel/"))
+)
+
+(when (file-exists-p (concat emacs-repo-elisp-submodule-dir "flim/"))
+  (add-to-list 'load-path (concat emacs-repo-elisp-submodule-dir "flim/"))
+)
+
+(when (file-exists-p (concat emacs-repo-elisp-submodule-dir "semi/"))
+  (add-to-list 'load-path (concat emacs-repo-elisp-submodule-dir "semi/"))
+)
