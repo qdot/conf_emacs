535d1caed4a063f3f883d70c940451b0925736dd
Kyle Machulis <kyle@nonpolynomial.com>
2010-06-15 21:52:34 -0700

    Fixed mailcap include issue in apel

--- emacs_conf_apel.el
+++ emacs_conf_apel.el
@@ -1,5 +1,11 @@
 ;; Setting up load directories for apel/flim/semi that a lot of japanese emacs modules use
 
+;; We must require mailcap /before/ flim to make sure we get the right one, 
+;; since it comes with a super old version
+
+
+(require 'mailcap)
+
 (when (file-exists-p (concat emacs-repo-elisp-submodule-dir "apel/"))
   (add-to-list 'load-path (concat emacs-repo-elisp-submodule-dir "apel/"))
 )
