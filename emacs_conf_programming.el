199cd58046bdb039bb78b774e71877d15e1c96df
Kyle Machulis <kyle@nonpolynomial.com>
2011-01-02 23:02:18 -0800

    Shuffled some programming stuff around

--- emacs_conf_programming.el
+++ emacs_conf_programming.el
@@ -5,7 +5,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; doxymacs mode for editing doxygen
-(require 'doxymacs)
 (add-hook 'c-mode-common-hook'doxymacs-mode)
 (defun my-doxymacs-font-lock-hook ()
   (if (or (eq major-mode 'c-mode) (eq major-mode 'c++-mode))
@@ -15,50 +14,18 @@
 (autoload 'wikipedia-mode "wikipedia-mode"
   "Major mode for editing documents in Wikipedia markup." t)
 
-(autoload 'php-mode "php-mode"
-  "PHP editing mode" t)
-
-(autoload 'cmake-mode "cmake-mode"
-  "CMakeLists editing mode" t)
-
 (autoload 'json "json"
   "JSON creation" t)
 
 (autoload 'js2-mode "js2"
   "JS2 Javascript Editing" t)
 
-(autoload 'lua-mode "lua-mode" 
-  "Lua editing mode." t)
-
-(load-library "flymake_cursor.el")
-
-(when (file-exists-p (concat emacs-repo-elisp-src-dir "haskell-mode/"))
-  (add-to-list 'load-path (concat emacs-repo-elisp-src-dir "haskell-mode/"))
-  (require 'haskell-mode)
-  (require 'inf-haskell)
-  (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-  (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
-  (add-hook 'haskell-mode-hook 'font-lock-mode)
-  (setq haskell-font-lock-symbols t)
-)
-
-;; file extension mode recognition
-(setq auto-mode-alist
-      (append
-       '(
-         ("\\.php$"  . php-mode)
-         ("\\.py$"   . python-mode)
-         ("\\.sql$"  . sql-mode)
-         ("\\.mode$" . cmake-mode)
-         ("\\.php$" . php-mode)
-         ("\\.wiki$" . wikipedia-mode)
-         ("\\.json$" . python-mode)
-         ("\\.cmake$" . cmake-mode)
-         ("\\.hs$" . haskell-mode)
-         ("CMakeLists\\.txt\\'" . cmake-mode)
-         ("ChangeLog\\.txt\\'" . change-log-mode)
-         ("\\.lua$" . lua-mode)
-         ) auto-mode-alist))
+(require 'haskell-mode)
+(require 'inf-haskell)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(add-hook 'haskell-mode-hook 'font-lock-mode)
+(setq haskell-font-lock-symbols t)
 
 ;; gdb/gud
 (setq gdb-many-windows t)
@@ -85,15 +52,6 @@
 ;;     (delete-trailing-whitespace)))
 ;; Changing too many files and making my commits messy
 ;; (add-hook 'before-save-hook 'my-trailing-whitespace-hook)
-(when (file-exists-p "~/.emacs_files/elisp_local/magit")
-  (add-to-list 'load-path (expand-file-name "~/.emacs_files/elisp_local/magit/"))
-  (require 'magit)
-  (if macosx-p
-      (custom-set-variables
-       '(magit-git-executable "/usr/local/git/bin/git")
-       )
-    )
-  )
 
 ;; eldoc mode for showing function calls in mode line
 (setq eldoc-idle-delay 0)
@@ -147,4 +105,21 @@
       (smerge-mode 1))))
 
 (add-hook 'find-file-hook 'sm-try-smerge t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Super simple generic mode for nsis editing
+;; God forbid I have to do that often
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define-generic-mode 'nsis-generic-mode
+  nil ;;'(";")
+  '("Section" "SectionEnd" "Function" "FunctionEnd" "Call" "Goto")
+  '(("!\\([A-Za-z]+\\)" (1 'font-lock-builtin-face))
+    ("$[({]?\\([A-Za-z0-9_]+\\)[)}]?" (1 'font-lock-variable-name-face))
+    )
+  (list "\\.\\(nsi\\|nsh\\)$")    
+  nil
+  "Generic mode for nsis files.")
 
