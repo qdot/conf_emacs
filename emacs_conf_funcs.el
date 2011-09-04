088dc2e4e83a5727259e51028a82f8cc933f3fbf
Kyle <kyle@nonpolynomial.com>
2011-09-03 22:39:46 -0700

    Added smart tab functions picked up off reddit, even though not using it yet

--- emacs_conf_funcs.el
+++ emacs_conf_funcs.el
@@ -252,3 +252,13 @@ strings"
         (delete-file filename)
         (kill-buffer buffer)
         (message "File '%s' successfully removed" filename)))))
+
+;; http://www.reddit.com/r/emacs/comments/jfrqm/what_does_your_tab_key_do/
+(defun smart-tab ()
+  "If mark is active, indents region. Else if point is at the end of a symbol,
+   expands it. Else indents the current line. Acts as normal in minibuffer."
+  (interactive)
+  (cond (mark-active (indent-region (region-beginning) (region-end)))
+        ((and (looking-at "\\_>") (not (looking-at "end")))
+         (hippie-expand nil))
+        (t (indent-for-tab-command))))
