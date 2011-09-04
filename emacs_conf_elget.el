1cab9b7ecab3f521189bdebeffeaab542435e09d
Kyle <kyle@nonpolynomial.com>
2011-09-03 22:24:31 -0700

    Cleaned up elget conf to work with 3.0

--- emacs_conf_elget.el
+++ emacs_conf_elget.el
@@ -4,69 +4,10 @@
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; So the idea is that you copy/paste this code into your *scratch* buffer,
-;; hit C-j, and you have a working el-get.
-
-(setq el-get-sources
-      '(
-	;; (:name quick-yes
-	;;        :type http
-	;;        :url "ftp://download.tuxfamily.org/user42/quick-yes.el")
-
-	;; lua-mode 
-	;; mo-git-blame
-	;; offlineimap
-	;; osc
-	;; pycomplete+
-	;; ropemacs
-	;; slime
-	;; twittering-mode
-	;; wanderlust
-	;; xml-parse
-	auto-complete
-	cedet
-	cmake-mode 
-	color-theme
-	dired-single
-	doxymacs
-	el-get
-	emacs-w3m
-	erc-extras
-	erc-highlight-nicknames
-	filladapt
-	flymake-point
-	gnus-gravatar
-	google-maps
-	google-weather
-	gravatar 
-	haskell-mode
-	highlight-parentheses
-	icomplete+
-	ido-hacks
-	ioccur
-	magit
-	markdown-mode
-	mingus
-	nxhtml
-	org-buffers
-	org-mode
-	pastebin
-	processing-mode
-	pylookup
-	pymacs
-	python
-	rainbow-delimiters
-	smex
-	sml-modeline
-	todochiku
-	undo-tree
-	workgroups
-	yasnippet
-	))
-
 (add-to-list 'load-path (expand-file-name (concat qdot/emacs-autoinst-elisp-dir "el-get")))
 (setq el-get-dir qdot/emacs-autoinst-elisp-dir)
 (setq el-get-verbose t)
+(setq el-get-status-file (expand-file-name (concat qdot/emacs-conf-dir "elget-status.el")))
 
 (unless (require 'el-get nil t)
   (url-retrieve
@@ -74,11 +15,5 @@
    (lambda (s)
      (end-of-buffer)
      (eval-print-last-sexp))))
-
-(add-to-list 'el-get-recipe-path (expand-file-name (concat qdot/emacs-autoinst-elisp-dir "el-get/recipes/")))
-(setq el-get-status-file (expand-file-name (concat qdot/emacs-conf-dir "elget-status.el")))
-
-(loop for src in el-get-sources
-      do (add-to-list 'load-path (expand-file-name (concat qdot/emacs-autoinst-elisp-dir (el-get-source-name src)))))
 
 (el-get)
