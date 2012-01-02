a2186ee795e4e4c0f58117f926259851da8128f0
Kyle <kyle@nonpolynomial.com>
2012-01-01 18:14:55 -0800

    Fixing buffer list sorting in workgroups to not lose buffer history ordering Indentation fixes

--- emacs_conf_wg.el
+++ emacs_conf_wg.el
@@ -10,7 +10,7 @@
 (defun qdot/wg-filter-buffer-list-by-not-major-mode (major-mode buffer-list)
   "Return only those buffers in BUFFER-LIST in major-mode MAJOR-MODE."
   (remove-if (lambda (mm) (eq mm major-mode))
-			 buffer-list :key 'wg-buffer-major-mode))
+	     buffer-list :key 'wg-buffer-major-mode))
 
 (defun qdot/wg-filter-buffer-list-by-erc-query (server buffer-list)
   "Return only those buffers in BUFFER-LIST in major-mode MAJOR-MODE."
@@ -23,13 +23,14 @@
 
 (defun qdot/wg-buffer-list-filter-associated-not-irc (workgroup buffer-list)
   "Return only those buffers in BUFFER-LIST in `erc-mode'."
-  (qdot/wg-filter-buffer-list-by-not-major-mode 'erc-mode (wg-buffer-list-filter-associated workgroup buffer-list)))
+  (qdot/wg-filter-buffer-list-by-not-major-mode
+   'erc-mode (wg-buffer-list-filter-associated workgroup buffer-list)))
 
 
 (defun qdot/wg-buffer-list-filter-erc-channel (workgroup buffer-list)
   "Return only those buffers in BUFFER-LIST in `erc-mode'."
-  (wg-filter-buffer-list-by-regexp "^#"
-				   (wg-filter-buffer-list-by-major-mode 'erc-mode buffer-list)))
+  (wg-filter-buffer-list-by-regexp 
+   "^#" (wg-filter-buffer-list-by-major-mode 'erc-mode buffer-list)))
 
 (defun qdot/wg-buffer-list-filter-erc-query (workgroup buffer-list)
   "Return only those buffers in BUFFER-LIST in `erc-mode'."
@@ -47,13 +48,26 @@
 
 (add-to-list
  'wg-buffer-list-filter-definitions
- '(qdot/associated-not-irc "qdot/associated-not-irc" qdot/wg-buffer-list-filter-associated-not-irc))
+ '(qdot/associated-not-irc "qdot/associated-not-irc" 
+			   qdot/wg-buffer-list-filter-associated-not-irc))
 
 (defun qdot/wg-set-buffer-lists ()
-  (wg-set-workgroup-parameter (wg-get-workgroup "work") 'wg-buffer-list-filter-order-alist '((default qdot/associated-not-irc qdot/not-irc all)))
-  (wg-set-workgroup-parameter (wg-get-workgroup "erc") 'wg-buffer-list-filter-order-alist '((default qdot/erc-irc all)))
-  (wg-set-workgroup-parameter (wg-get-workgroup "bitlbee") 'wg-buffer-list-filter-order-alist '((default qdot/erc-query all))))
+  ;; (wg-set-workgroup-parameter (wg-get-workgroup "work") 
+  ;; 			      'wg-buffer-list-filter-order-alist 
+  ;; 			      '((default qdot/associated-not-irc qdot/not-irc all)))
+  (wg-set-workgroup-parameter (wg-get-workgroup "erc") 
+			      'wg-buffer-list-filter-order-alist 
+			      '((default qdot/erc-irc all)))
+  (wg-set-workgroup-parameter (wg-get-workgroup "bitlbee") 
+			      'wg-buffer-list-filter-order-alist 
+			      '((default qdot/erc-query all))))
 
 ;; (wg-filter-buffer-list-by-major-mode 'erc-mode (buffer-list))
 ;; (wg-filter-buffer-list-by-not-major-mode 'erc-mode (buffer-list))
+
+;; Auto-place IM windows when switching to bitlbee workgroup
+(add-to-list 'wg-switch-to-workgroup-hook 
+	     (lambda () 
+	       (if (eq (wg-get-workgroup "bitlbee") (wg-current-workgroup))
+		   (qdot/bitlbee-resume-layout))))
 
