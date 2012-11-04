9a65ea2aa9101216bb323750827c3c6e8007c31e
Kyle Machulis <kyle@nonpolynomial.com>
2012-11-03 23:12:40 -0700

    Add expand region bind

--- emacs_conf_binds.el
+++ emacs_conf_binds.el
@@ -22,6 +22,7 @@
 (global-set-key (kbd "M-g s") 'magit-status)
 (global-set-key (kbd "C-x C-p") 'previous-multiframe-window)
 (global-set-key (kbd "C-x C-n") 'next-multiframe-window)
+(global-set-key (kbd "C-=") 'er/expand-region)
 
 ;; Bind smex over M-x, deals with sorting most used commands to front of IDO
 (global-set-key (kbd "M-x") 'smex)
