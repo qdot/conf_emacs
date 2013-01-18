109154e529da72d46e420517e149fbc9065e3e06
Kyle Machulis <kyle@nonpolynomial.com>
2013-01-18 12:17:26 -0800

    org'd everything. Moved most everything into setup for the moment just to see what having one big file would be like.

--- init.el
+++ init.el
@@ -3,14 +3,10 @@
 ;; the head of org-mode which we may have in an odd place.
 ;;
 
-;; load the starter kit from the `after-init-hook' so all packages are loaded
+;; load from the `after-init-hook' so all packages are loaded
 (add-hook 'after-init-hook
  `(lambda ()
-    ;; remember this directory
-    (setq qdot/emacs-conf-dir ,(file-name-directory (or load-file-name (buffer-file-name))))
-    ;; load up the starter kit
-		(add-to-list 'load-path (concat qdot/emacs-conf-dir "elisp_auto/org-mode"))
     (require 'org)
-    (org-babel-load-file (expand-file-name "emacs_conf.org" qdot/emacs-conf-dir))))
+    (org-babel-load-file (expand-file-name "emacs_conf.org" ,(file-name-directory (or load-file-name (buffer-file-name)))))))
 
 ;;; init.el ends here
