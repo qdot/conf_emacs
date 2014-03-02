64bb78cdb556f7565b5dec0b9d369efa90dd25a7
Kyle Machulis <kyle@nonpolynomial.com>
2014-03-02 14:16:02 -0800

    Turning on debug-on-error around init

--- init.el
+++ init.el
@@ -6,10 +6,12 @@
 ;; load from the `after-init-hook' so all packages are loaded
 (add-hook 'after-init-hook
 	  `(lambda ()
+	     (toggle-debug-on-error)
 	     ;; Make sure we load our org-mode checkout first, otherwise shit happens.
 	     (add-to-list 'load-path "~/.emacs_files/elisp_auto/org-mode/lisp/")
 	     (require 'cl)
 	     (require 'org)
-       (org-babel-load-file "~/.emacs_files/emacs_conf.org"))
+	     (org-babel-load-file "~/.emacs_files/emacs_conf.org")
+	     (toggle-debug-on-error)))
 
 ;;; init.el ends here
