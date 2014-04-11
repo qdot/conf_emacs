0594ef72af77188e898e817ca5cadb1fd23a63da
Kyle Machulis <kyle@nonpolynomial.com>
2014-04-10 19:42:01 -0700

    Remove org-schedule

--- org-schedule/org_daily_kyle.el
+++
@@ -1,6 +0,0 @@
-(load-file "~/.emacs_files/org-schedule/emacs_org_only.el")
-
-(defun qdot/daily-agenda-kyle ()
-	(org-batch-agenda " " org-agenda-ndays 2 org-agenda-include-diary nil))
-
-(qdot/daily-agenda-kyle)
--- org-schedule/org_weekly_kyle.el
+++
@@ -1,7 +0,0 @@
-(load-file "emacs_org_only.el")
-
-;; (defun qdot/weekly-agenda-kyle ()
-;;   (org-batch-agenda " " org-agenda-ndays 7 org-agenda-include-diary nil))
-
-;; (qdot/weekly-agenda-kyle)
-(org-store-agenda-views)
