99f3e15d43acfd3e18dbd3371bd04f4a705014d3
Kyle Machulis <kyle@nonpolynomial.com>
2014-08-10 13:33:02 -0700

    New function from bernard's org-mode config

--- elisp/org-bh.el
+++ elisp/org-bh.el
@@ -333,6 +333,29 @@ When not restricted, skip project and sub-project tasks, habits, and project rel
   (when (member-ignore-case tag qdot/org-auto-exclude-tags)
     (concat "-" tag)))
 
+(defun bh/skip-non-archivable-tasks ()
+  "Skip trees that are not available for archiving"
+  (save-restriction
+    (widen)
+    ;; Consider only tasks with done todo headings as archivable candidates
+    (let ((next-headline (save-excursion (or (outline-next-heading) (point-max))))
+          (subtree-end (save-excursion (org-end-of-subtree t))))
+      (if (member (org-get-todo-state) org-todo-keywords-1)
+          (if (member (org-get-todo-state) org-done-keywords)
+              (let* ((daynr (string-to-int (format-time-string "%d" (current-time))))
+                     (a-month-ago (* 60 60 24 (+ daynr 1)))
+                     (last-month (format-time-string "%Y-%m-" (time-subtract (current-time) (seconds-to-time a-month-ago))))
+                     (this-month (format-time-string "%Y-%m-" (current-time)))
+                     (subtree-is-current (save-excursion
+                                           (forward-line 1)
+                                           (and (< (point) subtree-end)
+                                                (re-search-forward (concat last-month "\\|" this-month) subtree-end t)))))
+                (if subtree-is-current
+                    subtree-end ; Has a date in this month or last month, skip it
+                  nil))  ; available to archive
+            (or subtree-end (point-max)))
+        next-headline))))
+
 (setq org-agenda-auto-exclude-function 'qdot/org-auto-exclude-function)
 
 (provide 'org-bh)
